01b710299a1575c868f8c4a239dee9ec1928bafe
pholmes10 <pholmes10@gmail.com>
2019-01-23 23:05:35 -0600

    configured home page react app

---
+++ nytreact/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+          <h1>this is my home page</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
